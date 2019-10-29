99f77685637bdb99c7435f68f48412e10aafe1db
InfactRepo <56988077+InfactRepo@users.noreply.github.com>
2019-10-29 15:17:54 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,7 @@ var sql = require("mssql");
     // Configuration object for your database
     var config = {
         user: 'sa',
-        password: 'sam@cinglevue123',
+        password: '',
         server: 'localhost', 
         database: 'sample_crud' 
     };
